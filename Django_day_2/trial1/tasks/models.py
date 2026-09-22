358718f81a980b8aeecf85e1e5847ef5a18fb886
PhilipOndieki <philipbarongo30@gmail.com>
2026-09-22 09:36:12 +0300

    added serializers

---
+++ Django_day_2/trial1/tasks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class TasksList(models.Models):
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
